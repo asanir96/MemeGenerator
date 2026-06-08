56e67a29ecdf65da3b1074c424f385ff2947abfd
asanir96 <asanir96@gmail.com>
2026-06-08 06:49:32 +0300

    Init memeController and basic image rendering to canvas

--- js/controllers/memeController.js
+++ js/controllers/memeController.js
@@ -1,7 +1,30 @@
 'use strict'
 
-var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]
+var gImgs = [
+    { id: 1, url: '/images/meme-imgs (various aspect ratios)/2.jpg', keywords: ['funny'] },
+    { id: 2, url: 'images/meme-imgs (various aspect ratios)/003.jpg', keywords: ['funny', 'trump'] }
+]
+var gElMemeCanvas
+var gMemeCtx
 
-function renderMeme(){
-    
+function onInit() {
+    renderMeme()
 }
+function renderMeme() {
+    gElMemeCanvas = document.querySelector('canvas')
+    gMemeCtx = gElMemeCanvas.getContext('2d')
+
+    const img = new Image()
+    console.log(img)
+    img.onload = () => renderImg(img)
+    img.src = gImgs[0].url
+
+}
+
+function renderImg(img) {
+    gElMemeCanvas.height = (img.naturalHeight / img.naturalWidth) * gElMemeCanvas.width
+    gMemeCtx.drawImage(img, 0, 0, gElMemeCanvas.width, gElMemeCanvas.height)
+
+    gMemeCtx.fillText("Hello world", 10, 50);
+}
+
